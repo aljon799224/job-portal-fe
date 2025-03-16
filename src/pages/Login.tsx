3e9d0bdb7a6ec8dfa8da2f6e05ffd25a6d404483
Aljon Mendiola <aljonmendiola@Aljons-Mac-mini.local>
2025-03-16 19:40:28 +0800

    setup authentication

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,162 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useAutoHideToast } from "../hooks/useAutoHideToast";
+import axios from "axios";
 
 const Login: React.FC = () => {
 	const [formData, setFormData] = useState({
-		email: "",
+		username: "",
 		password: "",
 	});
+
+	const [errorMessage, setErrorMessage] = useState<string | null>(null);
+	const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+	const [isToastVisible, setIsToastVisible] = useState(false);
+
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	// Automatically hide toast after 3 seconds
+	useAutoHideToast(isToastVisible, setIsToastVisible);
+
+	useEffect(() => {
+		if (location.state?.message) {
+			setSuccessMessage(location.state.message);
+			setIsToastVisible(true);
+			navigate(location.pathname, { replace: true, state: { message: null } });
+		}
+	}, [location.state, navigate]);
+
+	const closeToast = () => setIsToastVisible(false);
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
 
-	const handleSubmit = (e: React.FormEvent) => {
+	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
 		console.log("Logging in:", formData);
-		// TODO: Connect to backend API
+		try {
+			const response = await axios.post(
+				"http://localhost:8000/api/v1/auth/login/token",
+				formData,
+				{
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded",
+					},
+				}
+			);
+			const token = response.data.access_token;
+			localStorage.setItem("token", token);
+
+			navigate("/", { state: { message: "Login Successful!" } });
+		} catch (error) {
+			setSuccessMessage("");
+			setErrorMessage("Invalid username or password.");
+			setIsToastVisible(true);
+		}
 	};
 
 	return (
 		<div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
 			<div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
+				{/* toast */}
+
+				{isToastVisible && errorMessage && (
+					<div
+						id="toast-danger"
+						className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow mx-auto"
+						role="alert"
+					>
+						<div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg">
+							<svg
+								className="w-5 h-5"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+							>
+								<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+							</svg>
+							<span className="sr-only">Error icon</span>
+						</div>
+						<div className="ms-3 text-sm font-normal">{errorMessage}</div>
+						<button
+							type="button"
+							className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 text-gray-500"
+							data-dismiss-target="#toast-danger"
+							aria-label="Close"
+							onClick={closeToast}
+						>
+							<span className="sr-only">Close</span>
+							<svg
+								className="w-3 h-3"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 14 14"
+							>
+								<path
+									stroke="currentColor"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+								/>
+							</svg>
+						</button>
+					</div>
+				)}
+
+				{isToastVisible && successMessage && (
+					<div
+						id="toast-danger"
+						className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow mx-auto"
+						role="alert"
+					>
+						<div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+							<svg
+								className="w-5 h-5"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+							>
+								<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+							</svg>
+							<span className="sr-only">Check icon</span>
+						</div>
+						<div className="ms-3 text-sm font-normal">{successMessage}</div>
+						<button
+							type="button"
+							className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 text-gray-500"
+							data-dismiss-target="#toast-danger"
+							aria-label="Close"
+							onClick={closeToast}
+						>
+							<span className="sr-only">Close</span>
+							<svg
+								className="w-3 h-3"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 14 14"
+							>
+								<path
+									stroke="currentColor"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+								/>
+							</svg>
+						</button>
+					</div>
+				)}
+
+				{/* end toast */}
+
 				<h2 className="text-2xl font-bold text-center text-[#ff7409]">Login</h2>
 				<p className="text-gray-600 text-center mb-4">
 					Sign in to your account
@@ -27,11 +164,11 @@ const Login: React.FC = () => {
 
 				<form onSubmit={handleSubmit} className="space-y-4">
 					<div>
-						<label className="block text-sm font-medium">Email Address</label>
+						<label className="block text-sm font-medium">Username</label>
 						<input
-							type="email"
-							name="email"
-							value={formData.email}
+							type="username"
+							name="username"
+							value={formData.username}
 							onChange={handleChange}
 							className="w-full p-2 border rounded"
 							required
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,26 +1,116 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register: React.FC = () => {
+	const navigate = useNavigate();
+
+	const [errorMessage, setErrorMessage] = useState<string | null>(null);
+	const [isToastVisible, setIsToastVisible] = useState(false);
+
 	const [formData, setFormData] = useState({
-		name: "",
+		userName: "",
+		firstName: "",
+		middleName: "",
+		lastName: "",
 		email: "",
 		password: "",
 		confirmPassword: "",
 	});
 
+	const closeToast = () => setIsToastVisible(false);
+
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
 
-	const handleSubmit = (e: React.FormEvent) => {
+	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
-		console.log("Registering user:", formData);
-		// TODO: Connect to backend API
+
+		if (formData.password !== formData.confirmPassword) {
+			setErrorMessage("Password do not match.");
+			setIsToastVisible(true);
+			return;
+		}
+
+		try {
+			const backendPayload = {
+				username: formData.userName,
+				email: formData.email,
+				first_name: formData.firstName,
+				middle_name: formData.middleName,
+				last_name: formData.lastName,
+				password: formData.password,
+			};
+
+			const response = await axios.post(
+				"http://localhost:8000/api/v1/user",
+				backendPayload
+			);
+
+			console.log(response);
+			navigate("/login", { state: { message: "Registration Successful!" } });
+		} catch (error: any) {
+			setErrorMessage(
+				"The username and password must be unique. Please ensure they are not already in use or verify your internet connection and try again."
+			);
+			setIsToastVisible(true);
+			console.error(error);
+		}
 	};
 
 	return (
 		<div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
 			<div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
+				{/* toast */}
+
+				{isToastVisible && (
+					<div
+						id="toast-danger"
+						className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow mx-auto"
+						role="alert"
+					>
+						<div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg">
+							<svg
+								className="w-5 h-5"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="currentColor"
+								viewBox="0 0 20 20"
+							>
+								<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+							</svg>
+							<span className="sr-only">Error icon</span>
+						</div>
+						<div className="ms-3 text-sm font-normal">{errorMessage}</div>
+						<button
+							type="button"
+							className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 text-gray-500"
+							data-dismiss-target="#toast-danger"
+							aria-label="Close"
+							onClick={closeToast}
+						>
+							<span className="sr-only">Close</span>
+							<svg
+								className="w-3 h-3"
+								aria-hidden="true"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 14 14"
+							>
+								<path
+									stroke="currentColor"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+								/>
+							</svg>
+						</button>
+					</div>
+				)}
+
+				{/* end toast */}
 				<h2 className="text-2xl font-bold text-center text-[#ff7409]">
 					Register
 				</h2>
@@ -30,11 +120,47 @@ const Register: React.FC = () => {
 
 				<form onSubmit={handleSubmit} className="space-y-4">
 					<div>
-						<label className="block text-sm font-medium">Full Name</label>
+						<label className="block text-sm font-medium">Username</label>
 						<input
 							type="text"
-							name="name"
-							value={formData.name}
+							name="userName"
+							value={formData.userName}
+							onChange={handleChange}
+							className="w-full p-2 border rounded"
+							required
+						/>
+					</div>
+
+					<div>
+						<label className="block text-sm font-medium">First Name</label>
+						<input
+							type="text"
+							name="firstName"
+							value={formData.firstName}
+							onChange={handleChange}
+							className="w-full p-2 border rounded"
+							required
+						/>
+					</div>
+
+					<div>
+						<label className="block text-sm font-medium">Middle Name</label>
+						<input
+							type="text"
+							name="middleName"
+							value={formData.middleName}
+							onChange={handleChange}
+							className="w-full p-2 border rounded"
+							required
+						/>
+					</div>
+
+					<div>
+						<label className="block text-sm font-medium">Last Name</label>
+						<input
+							type="text"
+							name="lastName"
+							value={formData.lastName}
 							onChange={handleChange}
 							className="w-full p-2 border rounded"
 							required
